255d169bf7114606db872a8e2bffcff4565244a9
Francis Ferri <francis.ferri@epn.edu.ec>
2020-07-24 16:08:28 -0500

    TypeORM
    Conectar a MySQL

--- 01-http/introduccion/src/usuario/usuario.service.ts
+++ 01-http/introduccion/src/usuario/usuario.service.ts
@@ -1,6 +1,16 @@
 import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class UsuarioService{
-
+    constructor( // Inyeccion de dependecias
+        @InjectRepository(UsuarioEntity)
+        private repositorio: Repository<UsuarioEntity>
+    ) {
+    }
+    crearUno(nuevoUsuario:UsuarioEntity){
+        return this.repositorio.save(nuevoUsuario)
+    }
 }
