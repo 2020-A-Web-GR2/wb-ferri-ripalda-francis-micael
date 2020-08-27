c061582364d89f2b3fc441bae0420b1135ce53ce
Francis Ferri <francis.ferri@epn.edu.ec>
2020-08-27 16:07:48 -0500

    Paginas estaticas e instalacion bootstrap

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -10,7 +10,7 @@ import {VacunaModule} from "./vacuna/vacuna.module";
 import {MascotaEntity} from "./mascota/mascota.entity";
 import {VacunaEntity} from "./vacuna/vacuna.entity";
 
-const IP = "104.154.204.25";
+const IP = "35.194.50.214";
 
 
 @Module({
